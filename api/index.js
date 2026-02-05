1a52a6c5ea39a090a8e7d32baf900bc836a8ea4b
Rani <raniachheylal24@navgurukul.org>
2026-02-05 12:08:05 +0530

    Fix Vercel config for proper serverless deployment

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
-// Vercel serverless function
+// Vercel serverless function handler
 const app = require('../server/server');
 
+// Export the Express app as serverless function
 module.exports = app;
